589e9bde8874e902814fd9ede763b7ede168f9d7
Khaibr984 <khaibr984@edu.linkoping.se>
2025-04-29 08:58:43 +0000

    2025-04-29

---
+++ projekt/java.js
@@ -0,0 +1,14 @@
+const annoying = document.querySelector(".annoying");
+const Button = document.querySelector(".Button");
+const khalid = document.querySelector(".khalid");
+
+Button.addEventListener("click", function (event) {
+    annoying.style.display = "none";
+  
+ 
+  });
+  Button.addEventListener("click", function (event) {
+    khalid.style.display = "none";
+  
+ 
+  });
