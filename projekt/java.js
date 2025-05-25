44a52af35c8ca2e52913eae2842a68ccec40c7f7
Khaibr984 <khaibr984@edu.linkoping.se>
2025-05-25 12:10:01 +0000

    slut

--- projekt/java.js
+++ projekt/java.js
@@ -4,11 +4,22 @@ const khalid = document.querySelector(".khalid");
 
 Button.addEventListener("click", function (event) {
     annoying.style.display = "none";
-  
- 
+     khalid.style.display = "none";
   });
-  Button.addEventListener("click", function (event) {
-    khalid.style.display = "none";
+
   
+let currentSize = 16; // Startstorlek i px
+
+function makeTextBigger() {
+  if (currentSize < 22)
+  currentSize += 2;
+  document.body.style.fontSize = currentSize + "px";
+}
+
+function makeTextSmaller() {
+  if (currentSize > 10) {
+    currentSize -= 2;
+    document.body.style.fontSize = currentSize + "px";
+  }
+}
  
-  });
